e8c282eb74a54b24feb5bc5328926b54e8ed9396
modelTR <1931292925@qq.com>
2026-09-22 20:40:03 +0800

    add comments
    Signed-off-by: modelTR <1931292925@qq.com>

--- err.hpp
+++ err.hpp
@@ -11,8 +11,8 @@
 
 #include <fmt/base.h>
 #include <fmt/format.h>
-#include <tl/expected.hpp>
 #include <spdlog/spdlog.h>
+#include <tl/expected.hpp>
 
 template <class T> using vec = std::vector<T>;
 
@@ -135,17 +135,18 @@ inline auto print() {
 	return [](Err &e) -> void { e.print(); };
 }
 
+/// @note specialized fmt::formatter<std::source_location> need two
+/// FMT_BEGIN_NAMESPACE and FMT_END_NAMESPACE to warpper
+
 FMT_BEGIN_NAMESPACE
 
-template<>
-struct fmt::formatter<std::source_location> {
-	constexpr auto parse(fmt::format_parse_context &ctx){
-		return ctx.begin();
-	}
+template <> struct fmt::formatter<std::source_location> {
+	constexpr auto parse(fmt::format_parse_context &ctx) { return ctx.begin(); }
 
-	template<class FormatContext>
-	auto format(const std::source_location &loc, FormatContext &ctx) const{
-		return fmt::format_to(ctx.out(), "{} : {}", loc.file_name(), loc.function_name());
+	template <class FormatContext>
+	auto format(const std::source_location &loc, FormatContext &ctx) const {
+		return fmt::format_to(ctx.out(), "{} : {}", loc.file_name(),
+							  loc.function_name());
 	}
 };
 
@@ -165,10 +166,8 @@ auto make_err(spdlog::format_string_t<Args...> fmt, Args &&...args) -> err {
 
 template <class T = std::source_location>
 auto get_fn_name(fmt::format_string<T> fmt = "Fn[\033[34m{}\033[0m]\n",
-                 T&& loc = std::source_location::current()) -> str {
-    return fmt::format(fmt, std::forward<T>(loc));
+				 T &&loc = std::source_location::current()) -> str {
+	return fmt::format(fmt, std::forward<T>(loc));
 }
-
-
 
 } // namespace util

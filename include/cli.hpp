ae13b0e79a4ef101e2459a6ee5ab769f87a962c6
modelTR <1931292925@qq.com>
2026-09-25 23:43:21 +0800

    refactor(CLI): Redefine factory function cli::make()
    make cli::make() more clear
    
    Signed-off-by: modelTR <1931292925@qq.com>

--- include/cli.hpp
+++ include/cli.hpp
@@ -2,26 +2,12 @@
 
 #include "cxxopts.hpp"
 
-#include "err.hpp"
 #include "type_alias.hpp"
-#include <filesystem>
 
 namespace cli {
 class CLI {
 public:
-	CLI(int argc, char **argv) {
-		opts_.add_options()("o,output", "Output Dictionary",
-							cxxopts::value<fs::path>());
-		auto res = opts_.parse(argc, argv);
-
-		if (!res.count("output")) {
-			spdlog::warn("No --output Given, Falling Back to CWD: {}",
-						 fs::current_path().string());
-
-		} else {
-			output_path_ = res["output"].as<fs::path>();
-		}
-	}
+	CLI(cxxopts::Options &&opts, fs::path &&op);
 	CLI() = delete;
 	CLI(const CLI &) = delete;
 	auto operator=(const CLI &) -> CLI & = delete;
--- src/cli.cpp
+++ src/cli.cpp
@@ -1,3 +1,27 @@
 #include "cli.hpp"
+#include "err.hpp"
+#include <cxxopts.hpp>
+#include <filesystem>
+#include <spdlog/spdlog.h>
+#include <utility>
 
-auto cli::make(int argc, char **argv) -> CLI { return CLI(argc, argv); }
+cli::CLI::CLI(cxxopts::Options &&opts, fs::path &&op)
+	: opts_(std::move(opts)), output_path_(std::move(op)) {}
+
+auto cli::make(int argc, char **argv) -> CLI {
+	cxxopts::Options opts{"fresh-spv", "Compress SPIR-V file significantly"};
+	fs::path op{};
+
+	opts.add_options()("o, output", "Output Dictionary",
+					   cxxopts::value<fs::path>());
+
+	auto res = opts.parse(argc, argv);
+
+	if (!res.count("output")) {
+		spdlog::warn("{} - \"output\" isn't specified", util::get_fn_name());
+	} else {
+		op = res["output"].as<fs::path>();
+	}
+
+	return CLI{std::move(opts), std::move(op)};
+}
